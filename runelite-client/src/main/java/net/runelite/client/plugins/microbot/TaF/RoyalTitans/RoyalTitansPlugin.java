12aec292c77fa0732587d92411bc58e00005c80f
Steffen Carlsen <SteffenCarlsen@taffe.com>
2025-04-17 03:33:48 +0200

    Initial implementation of Royal Titan script + update of Transportation_items.tsv with Giantsoul Amulet

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/RoyalTitans/RoyalTitansPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/RoyalTitans/RoyalTitansPlugin.java
@@ -5,23 +5,22 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.GraphicsObject;
 import net.runelite.api.GroundObject;
 import net.runelite.api.Tile;
-import net.runelite.api.TileObject;
-import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.*;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.microbot.Microbot;
-import net.runelite.client.plugins.microbot.util.containers.FixedSizeQueue;
 import net.runelite.client.plugins.microbot.util.player.Rs2Player;
-import net.runelite.client.plugins.microbot.util.tile.DangerousTileIds;
 import net.runelite.client.plugins.microbot.util.tile.Rs2Tile;
 import net.runelite.client.plugins.microbot.util.walker.Rs2Walker;
 import net.runelite.client.ui.overlay.OverlayManager;
 
 import javax.inject.Inject;
 import java.awt.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @PluginDescriptor(
         name = PluginDescriptor.Mocrosoft + "Royal Titans",
@@ -34,6 +33,7 @@ public class RoyalTitansPlugin extends Plugin {
     private final Integer GRAPHICS_OBJECT_FIRE =  3218;
     private final Integer GRAPHICS_OBJECT_ICE =  3221;
     private final Integer ENRAGE_ELEMENTAL_BLAST_SAFESPOT =  56003;
+    private ScheduledExecutorService scheduledExecutorService;
     @Inject
     private RoyalTitansConfig config;
 
@@ -46,22 +46,26 @@ public class RoyalTitansPlugin extends Plugin {
     private OverlayManager overlayManager;
     @Inject
     private RoyalTitansOverlay royalTitansOverlay;
-
     @Inject
     public RoyalTitansScript royalTitansScript;
 
     @Override
     protected void startUp() throws AWTException {
+        scheduledExecutorService = Executors.newScheduledThreadPool(50);
         if (overlayManager != null) {
             overlayManager.add(royalTitansOverlay);
         }
         royalTitansScript.run(config);
+        Rs2Tile.init();
     }
 
     @Override
     protected void shutDown() {
         royalTitansScript.shutdown();
         overlayManager.remove(royalTitansOverlay);
+        if (scheduledExecutorService != null && !scheduledExecutorService.isShutdown()) {
+            scheduledExecutorService.shutdown();
+        }
     }
 
     @Subscribe
@@ -69,16 +73,16 @@ public class RoyalTitansPlugin extends Plugin {
     {
         final GraphicsObject graphicsObject = event.getGraphicsObject();
         if (graphicsObject.getId() == GRAPHICS_OBJECT_ICE || graphicsObject.getId() == GRAPHICS_OBJECT_FIRE) {
-            Rs2Tile.addDangerousGraphicsObjectTile(graphicsObject, 600 * 5);
+            Rs2Tile.addDangerousGraphicsObjectTile(graphicsObject, 600 * 3);
         }
     }
 
     @Subscribe
     public void onGroundObjectDespawned(GroundObjectDespawned event) {
         final GroundObject groundObject = event.getGroundObject();
-        final Tile tile = event.getTile();
         if (groundObject.getId() == ENRAGE_ELEMENTAL_BLAST_SAFESPOT) {
             royalTitansScript.enrageTile = null;
+            Microbot.log("Enrage tile despawned");
         }
     }
 
@@ -87,20 +91,16 @@ public class RoyalTitansPlugin extends Plugin {
         final GroundObject groundObject = event.getGroundObject();
         final Tile tile = event.getTile();
         if (groundObject.getId() == ENRAGE_ELEMENTAL_BLAST_SAFESPOT) {
-            royalTitansScript.enrageTile = tile;
+            try {
+                royalTitansScript.enrageTile = tile;
+                scheduledExecutorService.schedule(() -> {
+                    if (!Rs2Player.getWorldLocation().equals(tile.getWorldLocation())) {
+                        Rs2Walker.walkFastCanvas(tile.getWorldLocation());
+                    }
+                }, 100, TimeUnit.MILLISECONDS);
+            } catch (Exception e) {
+                Microbot.log("Error while walking to enrage tile: " + e.getMessage());
+            }
         }
-    }
-
-
-
-    @Subscribe
-    public void onGameObjectSpawned(GameObjectSpawned gameObjectSpawned)
-    {
-        if (!Microbot.getClient().isInInstancedRegion())
-        {
-            return;
-        }
-
-        //royalTitansScript.gameObjectSpawned(gameObjectSpawned.getGameObject());
     }
 }
