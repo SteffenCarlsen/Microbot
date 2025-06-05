c2263d9e4155a77bff5946ccec4104b0bb3dc199
Steffen Carlsen <SteffenCarlsen@taffe.com>
2025-06-05 12:16:19 +0200

    Update crab locations

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/AmmoniteCrabs/enums/AmmoniteCrabLocations.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/TaF/AmmoniteCrabs/enums/AmmoniteCrabLocations.java
@@ -6,9 +6,8 @@ import net.runelite.api.coords.WorldPoint;
 public enum AmmoniteCrabLocations {
     NORTH_WEST("North West (3 spot)", new WorldPoint(3657, 3875, 0), new WorldPoint(3664, 3875, 0), new WorldPoint(3689, 3897, 0)),
     NORTH_EAST("North East (2 spot)", new WorldPoint(3718, 3881, 0), new WorldPoint(3708, 3874, 0), new WorldPoint(3690, 3874, 0)),
-    SOUTH_WEST("South West (3 spot)", new WorldPoint(3719, 3846, 0), new WorldPoint(3726, 3839, 0), new WorldPoint(3680, 3844, 0)),
-    SOUTH_EAST("South West (3 spot)", new WorldPoint(3734, 3846, 0), new WorldPoint(3734, 3839, 0), new WorldPoint(3736, 3818, 0));
-
+    SOUTH_WEST("South West (2 spot)", new WorldPoint(3717, 3846, 0), new WorldPoint(3726, 3839, 0), new WorldPoint(3680, 3844, 0)),
+    SOUTH_EAST("South East (2 spot)", new WorldPoint(3733, 3846, 0), new WorldPoint(3734, 3839, 0), new WorldPoint(3736, 3818, 0));
     @Getter
     private final String LocationName;
     @Getter
