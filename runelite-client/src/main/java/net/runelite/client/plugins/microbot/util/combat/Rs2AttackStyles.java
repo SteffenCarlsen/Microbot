6d528b778fbe7931e48de458ef3c8c1938f326be
Steffen Carlsen <SteffenCarlsen@taffe.com>
2025-06-10 20:06:08 +0200

    Implement attackstyle switching

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/combat/Rs2AttackStyles.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/combat/Rs2AttackStyles.java
@@ -2,9 +2,13 @@ package net.runelite.client.plugins.microbot.util.combat;
 
 import net.runelite.api.VarPlayer;
 import net.runelite.api.Varbits;
+import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.util.combat.AttackTypeHelpers.AttackType;
 import net.runelite.client.plugins.microbot.util.combat.AttackTypeHelpers.WeaponAttackType;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Rs2AttackStyles {
     /**
@@ -17,7 +21,7 @@ public class Rs2AttackStyles {
         final int currentAttackStyleVarbit = Microbot.getVarbitPlayerValue(VarPlayer.ATTACK_STYLE);
         final int currentEquippedWeaponTypeVarbit = Microbot.getVarbitValue(Varbits.EQUIPPED_WEAPON_TYPE);
         final int currentCastingModeVarbit = Microbot.getVarbitValue(Varbits.DEFENSIVE_CASTING_MODE);
-        return updateAttackStyle(currentEquippedWeaponTypeVarbit, currentAttackStyleVarbit, currentCastingModeVarbit);
+        return getAttackStyle(currentEquippedWeaponTypeVarbit, currentAttackStyleVarbit, currentCastingModeVarbit);
     }
 
     /**
@@ -31,7 +35,35 @@ public class Rs2AttackStyles {
         return getAttackTypesForCurrentWeapon(currentEquippedWeaponTypeVarbit);
     }
 
-    private static AttackType updateAttackStyle(int equippedWeaponType, int attackStyleIndex, int castingMode)
+    public static boolean setAttackStyle(AttackType attackType) {
+        final int currentEquippedWeaponTypeVarbit = Microbot.getVarbitValue(Varbits.EQUIPPED_WEAPON_TYPE);
+        final AttackType[] attackTypes = getAttackTypesForCurrentWeapon(currentEquippedWeaponTypeVarbit);
+        if (Arrays.stream(attackTypes).noneMatch(x -> Objects.equals(x.getName(), attackType.getName()))) {
+            Microbot.log("Attack style " + attackType.getName() + " not found in current weapon's attack types: " + Arrays.toString(attackTypes));
+            return false;
+        }
+        for (int i = 0; i < attackTypes.length; i++) {
+            if (Objects.equals(attackTypes[i].getName(), attackType.getName())) {
+                switch (i) {
+                    case 0:
+                        return Rs2Combat.setAttackStyle(WidgetInfo.COMBAT_STYLE_ONE);
+                    case 1:
+                        return Rs2Combat.setAttackStyle(WidgetInfo.COMBAT_STYLE_TWO);
+                    case 2:
+                        return Rs2Combat.setAttackStyle(WidgetInfo.COMBAT_STYLE_THREE);
+                    case 3:
+                        return Rs2Combat.setAttackStyle(WidgetInfo.COMBAT_STYLE_FOUR);
+                    default:
+                        return false;
+
+                }
+            }
+        }
+        Microbot.log("Attack style " + attackType.getName() + " not found in current weapon's attack types.");
+        return false;
+    }
+
+    private static AttackType getAttackStyle(int equippedWeaponType, int attackStyleIndex, int castingMode)
     {
         boolean isDefensiveCasting = castingMode == 1 && attackStyleIndex == 4;
         AttackType attackType = null;
